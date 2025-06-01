a75a2cbdf5e137c8922da61da2763d04ae7405cf
Malek Arafat <arafatmalek44@gmail.com>
2025-06-01 12:49:17 +0530

    error finding

--- Backend/DB/DB.js
+++ Backend/DB/DB.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+
 const connectDB = ()=>{
     mongoose.connect(process.env.MONGODB_URL,)
     .then(()=>{
--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,6 @@ const connectDB = require("./DB/DB");
 const cors = require("cors");
 const AIRoute = require("./route/AI");
 const cookieParser = require("cookie-parser");
-const path = require("path");
 
 const PORT=5000;
 
@@ -17,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/upload', express.static('upload'));
 
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
 app.use("/user",userRoute);
 app.use("/ai",AIRoute);
@@ -26,11 +24,6 @@ app.use("/ai",AIRoute);
 app.get("/",(req,res)=>{
     res.send("Backend is Running ")
 })
-
-// Catch-all: Serve index.html for frontend routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
-});
 
 
 app.listen(PORT,()=>{
