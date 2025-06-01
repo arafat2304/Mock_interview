54a6c00cdb272c3aa9c674302748cf71aa0951a7
Malek Arafat <arafatmalek44@gmail.com>
2025-06-01 14:08:20 +0530

    working with mongo atlas

--- Backend/DB/DB.js
+++ Backend/DB/DB.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = ()=>{
-    mongoose.connect(process.env.MONGODB_URL,{serverSelectionTimeoutMS: 50000})
+    mongoose.connect(process.env.MONGODB_URL,{serverSelectionTimeoutMS:30000})
     .then(()=>{
         console.log("Database Connected")
     }).catch((err)=>{
